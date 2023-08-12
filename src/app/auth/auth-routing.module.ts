39db3eec37a356186ed1c02704db5ffed2d9db73
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-12 19:42:03 +0200

    Paths a las rutas faltantes y introduccion de layout pages

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 //localhost:4200/auth
 const routes: Routes = [
   {
     path: "",
-    component: LayoutPageComponent
+    component: LayoutPageComponent,
+    children: [
+      {
+        path: "login",
+        component: LoginPageComponent
+      },
+      {
+        path: "new-account",
+        component: RegisterPageComponent
+      },
+      {
+        path: "**",
+        redirectTo: "login"
+      }
+    ]
   }
 ];
 
--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
 
 const routes: Routes = [
   {
@@ -11,7 +13,27 @@ const routes: Routes = [
       {
         path: "new-hero",
         component: NewPageComponent
-      }
+      },
+      {
+        path: "seach",
+        component: SearchPageComponent
+      },
+      {
+        path: "edit:/id",
+        component: NewPageComponent
+      },
+      {
+        path: "list",
+        component: ListPageComponent
+      },
+      {
+        path: ":id",
+        component: NewPageComponent
+      },
+      {
+        path: "**",
+        redirectTo: "list"
+      },
     ]
 
   }
