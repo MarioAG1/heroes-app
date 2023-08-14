21655964387ee23257ae70eb99e07672206526fb
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-14 20:28:54 +0200

    Traer informacion de los heroes con httprequest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Hero } from '../interfaces/hero.interface';
+import { environments } from 'src/environments/environments';
+
+@Injectable({ providedIn: 'root' })
+export class HeroesService {
+
+
+  private baseUrl:string = environments.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+
+   getHeroes():Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`);
+  }
+}
