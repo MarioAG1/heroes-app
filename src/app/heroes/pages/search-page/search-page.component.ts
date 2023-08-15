ea48df019609bc52582aa74f6c80cc9d5015b9ec
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-15 21:06:36 +0200

    Angular Material - Autocomplete PT2

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -7,11 +10,31 @@ import { FormControl } from '@angular/forms';
   styles: [
   ]
 })
-export class SearchPageComponent implements OnInit{
+export class SearchPageComponent {
 
- ngOnInit(): void {
+  public searchInput = new FormControl("")
+  public heroes: Hero[] = []
+  public selectedHero?: Hero
 
- }
+  constructor(private heroesService: HeroesService) {
 
- public searchInput = new FormControl("")
+  }
+
+  searchHero(): void {
+    const value: string = this.searchInput.value || ""
+    console.log({ value })
+    this.heroesService.getSuggestions(value)
+      .subscribe(heroes => this.heroes = heroes)
+  }
+
+  onSelectedOption(event: MatAutocompleteSelectedEvent): void {
+    if (!event.option.value) {
+      this.selectedHero = undefined
+      return
+    }
+    const hero: Hero = event.option.value
+    this.searchInput.setValue(hero.superhero)
+
+    this.selectedHero = hero
+  }
 }
