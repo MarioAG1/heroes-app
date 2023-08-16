2f52d803a0ee52536f87a60d53e7e44d7ff4ed16
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-16 20:29:33 +0200

    Conectar el formulario con el template

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
@@ -10,18 +10,25 @@ import { Publisher } from '../../interfaces/hero.interface';
 })
 export class NewPageComponent {
 
-  public heroForm = new FormControl({
-    id: new FormControl<string>(""),
+  public heroForm = new FormGroup({
+    id: new FormControl<string>('""'),
     superhero: new FormControl<string>("", { nonNullable: true }),
     publisher: new FormControl<Publisher>(Publisher.DCComics),
     alter_ego: new FormControl<string>(""),
     first_appearance: new FormControl<string>(""),
     characters: new FormControl<string>(""),
     alt_img: new FormControl<string>(""),
-  })
+  });
 
   public publishers = [
     { id: "Dc Comics", desc: "Dc - Comics" },
     { id: "Marvel Comics", desc: "Marvel - Comics" }
   ]
+
+  onSubmit(): void {
+    console.log({
+      formIsValid: this.heroForm.valid,
+      value: this.heroForm.value
+    })
+  }
 }
