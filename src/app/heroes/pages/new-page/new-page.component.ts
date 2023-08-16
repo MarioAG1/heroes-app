92729e4cd4bc024a732830f6cdcd3a71a751b3f8
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-16 21:37:20 +0200

    Getter dentro del componente

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Publisher } from '../../interfaces/hero.interface';
+import { Publisher, Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -25,10 +26,19 @@ export class NewPageComponent {
     { id: "Marvel Comics", desc: "Marvel - Comics" }
   ]
 
+  constructor(private heroesService: HeroesService) { }
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero
+    return hero
+  }
+
   onSubmit(): void {
-    console.log({
-      formIsValid: this.heroForm.valid,
-      value: this.heroForm.value
-    })
+
+    if (this.heroForm.valid) return
+
+    // this.heroesService.updateHero
+
+
   }
 }
--- src/app/heroes/pipes/hero-image.pipe.ts
+++ src/app/heroes/pipes/hero-image.pipe.ts
@@ -6,8 +6,9 @@ import { Hero } from '../interfaces/hero.interface';
 })
 export class HeroImagePipe implements PipeTransform {
 
-  transform(hero: Hero,): string {
-    if (!hero.id && !hero.alt_img) {
+  transform(hero: Hero): string {
+
+    if (!hero.alt_img) {
       return 'assets/no-image.png'
     }
 
