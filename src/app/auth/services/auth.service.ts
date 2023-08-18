6799c90d1f55f05546b34e884d9f2af18c94570a
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-18 19:02:37 +0200

    Mantener la sesion del usuario PT-1

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from 'src/environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, catchError, of, tap } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +18,8 @@ export class AuthService {
     if (!this.user) return undefined
     return structuredClone(this.user)
   }
+
+
   login(email: string, password: string): Observable<User | undefined> {
     // this.http.post("login", {email,pasword});
     return this.http.get<User>(`${this.baseUrl}/users/1`)
@@ -30,7 +32,22 @@ export class AuthService {
       )
   }
 
-  logout(){
+  checkAuthentication(): Observable<boolean> | boolean {
+    if (!localStorage.getItem("token")) return of(false)
+
+    const token = localStorage.getItem("token")
+
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+      .pipe(
+        tap(user => this.user = user),
+        map(user => !!user),
+        catchError( error => of(false))
+      );
+
+
+  }
+
+  logout() {
     this.user = undefined
     localStorage.clear()
   }
